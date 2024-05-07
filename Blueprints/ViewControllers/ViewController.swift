53a5d7202b793fad59d65077a0ac2904bb0336b9
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2024-05-07 15:21:58 +0500

    Initial Commit

---
+++ Blueprints/ViewControllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Blueprints
+//
+//  Created by Эльдар Абдуллин on 07.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
