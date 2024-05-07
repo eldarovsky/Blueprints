7e09e3ebb8fc89492fc679b5074ad8035880072e
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2024-05-08 01:04:20 +0500

    Did Minimal Viable Product.

---
+++ Blueprints/Extensions/UIView+.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+.swift
+//  Blueprints
+//
+//  Created by Эльдар Абдуллин on 07.05.2024.
+//
+
+import UIKit
+
+extension UIView {
+
+    /// Method for adding subviews
+    func addSubviews(_ views: UIView...) {
+        views.forEach { addSubview($0) }
+    }
+
+    /// Method for disabling autoresizing mask
+    func disableAutoresizingMask(_ views: UIView...) {
+        views.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+    }
+}
